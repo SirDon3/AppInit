f1770e914186d124407721f22a35b67c3ae0a926
SirDon3 <110694491+SirDon3@users.noreply.github.com>
2023-10-05 23:00:33 +0300

    phase one completed

--- app.js
+++ app.js
@@ -4,18 +4,36 @@ const express = require('express');
 // express app
 const app = express(); 
 
+// register view engine 
+app.set('view engine', 'ejs')
+
+// listen for requests 
+app.listen(3000);
 
 // routes
 app.get('/', (req, res) => {
-    res.send('<p> Hello Shda </p>')
+    // res.send('<p> Hello Shda </p>')
+    const blogs = [
+        {title: 'Lets get it started', snippet: 'Lorem ipsum dolor sit amet'},
+        {title: 'Trying something new', snippet: 'Lorem ipsum dolor sit amet'},
+        {title: 'New is the old', snippet: 'Lorem ipsum dolor sit amet'},
+    ]
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.send('<p> Hello about </p>')
+    // res.send('<p> About page </p>')
+    res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    // res.send('<p> About page </p>')
+    res.render('create', { title: 'New Blog' } );
 });
 
 
-// listen for requests 
-app.listen(3000, () =>{
-    console.log('listening on port 3000')
-});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' })
+})
